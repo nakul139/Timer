a64b15712ba16af7851733eb79672da4ce98ba9b
Nakul Jain <163165553+nakul139@users.noreply.github.com>
2024-10-06 19:45:10 +0530

    Update index.js

--- src/components/Clock/index.js
+++ src/components/Clock/index.js
@@ -2,10 +2,7 @@ import {Component} from 'react'
 import './index.css'
 
 class Clock extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {date: new Date()}
-  }
+  state = {date: new Date()}
 
   componentDidMount() {
     this.timerId = setInterval(this.tick, 1000)
